28b62ab6bcbcbf67547182afce1ea900be7d11df
Denislav Trifonov <denislav.trifonov.1995@gmail.com>
2021-03-02 16:23:08 +0200

    Add files via upload

--- SmartShelf/appinfomodel.cpp
+++ SmartShelf/appinfomodel.cpp
@@ -1,11 +1,12 @@
 #include "appinfomodel.h"
-#include <QJsonObject>
+#include <QDebug>
 #include <QFile>
-#include <QJsonDocument>
 #include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QObject>
 
-AppInfoModel::AppInfoModel(QObject *parent) : QAbstractListModel(parent)
+AppInfoModel::AppInfoModel(QObject* parent) : QAbstractListModel(parent)
 {
     // Get the data from .json file
     QString strJsonFile;
@@ -15,36 +16,58 @@ AppInfoModel::AppInfoModel(QObject *parent) : QAbstractListModel(parent)
     strJsonFile = oJsonFile.readAll();
     oJsonFile.close();
 
-    QJsonDocument oJsonDoc  = QJsonDocument::fromJson(strJsonFile.toUtf8());
-    QJsonObject oJsonObject = oJsonDoc.object();
-    QJsonArray arrJson = oJsonObject.value(QLatin1String("info"));
-    //QJsonValue value = sett2.value(QString("info"))
-    qWarning() << arrJson.value("info");
+    QJsonDocument oJsonDoc    = QJsonDocument::fromJson(strJsonFile.toUtf8());
+    QJsonObject oJsonObject   = oJsonDoc.object();
+    QJsonValue oJsonFileValue = oJsonObject.value("info");
+    QJsonArray arrAppInfo     = oJsonFileValue.toArray();
+
+    // fill the model
+    beginResetModel();
+
+    for (AppInfoItem* pMenuItem : m_arrAppInfoItems)
+    {
+        pMenuItem->deleteLater();
+    }
+    m_arrAppInfoItems.clear();
+
+    for (quint8 i = 0; i < arrAppInfo.size(); i++)
+    {
+        QJsonObject currentAppInfo       = arrAppInfo.at(i).toObject();
+        AppInfoItem* pCurrentAppInfoItem = new AppInfoItem(this, currentAppInfo.value("name").toString(), currentAppInfo.value("value").toString());
+        m_arrAppInfoItems.insert(i, pCurrentAppInfoItem);
+    }
+    endResetModel();
 }
 
-int AppInfoModel::rowCount(const QModelIndex &parent) const
+
+int AppInfoModel::rowCount(const QModelIndex& parent) const
 {
     Q_UNUSED(parent);
     return m_arrAppInfoItems.count();
 }
 
-QVariant AppInfoModel::data(const QModelIndex &index, int role) const
+QVariant AppInfoModel::data(const QModelIndex& index, int role) const
 {
     if (index.row() < 0 || index.row() >= m_arrAppInfoItems.count())
     {
         return QVariant();
     }
 
-    AppInfoItem currentAppInfoItem = m_arrAppInfoItems.at(index.row());
+    AppInfoItem* currentAppInfoItem = m_arrAppInfoItems.at(index.row());
 
     if (role == InfoTypeRole)
     {
-        return currentAppInfoItem.GetAppInfoType();
+        return currentAppInfoItem->GetAppInfoType();
     }
 
     else if (role == InfoRole)
     {
-        return currentAppInfoItem.GetAppInfo();
+        return currentAppInfoItem->GetAppInfo();
+    }
+
+    else if (role == ObjectRole)
+    {
+        return QVariant::fromValue(currentAppInfoItem);
     }
 
     return QVariant();
@@ -54,6 +77,6 @@ QHash<int, QByteArray> AppInfoModel::roleNames() const
 {
     QHash<int, QByteArray> roles;
     roles[InfoTypeRole] = "type";
-    roles[InfoRole] = "value";
+    roles[InfoRole]     = "value";
     return roles;
 }
